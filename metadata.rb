98413f0b49e2474c5f17ae7576d50eb7f6662ada
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-09-05 13:31:50 +0100

    corrected install oredering of packages

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs/Configures manta'
 source_url 'https://github.com/EagleGenomics-cookbooks/manta'
 issues_url 'https://github.com/EagleGenomics-cookbooks/manta/issues'
 
-version '1.0.0'
+version '1.0.1'
 
 depends 'build-essential'
 depends 'apt'
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'build-essential'
 include_recipe 'apt'
+include_recipe 'build-essential'
 
 package ['tar', 'zlib1g-dev'] do
   action :install
